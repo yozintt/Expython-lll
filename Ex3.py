81091e533ddeccbefd1b8b06b19ab2bbb7ad4c82
YOANDER FARLLEY LAEBER JUSTESEN <90282664+yozintt@users.noreply.github.com>
2023-09-16 21:40:05 +0200

    Update Ex3.py
    exercício atualizado com o meu irmão Johannes Justese

--- Ex3.py
+++ Ex3.py
@@ -1,17 +1,34 @@
-# Escreva um algoritmo que peça ao usuário 10 números, calcule e mostre a média desses 
-#10 números e mostre ainda quantos, desses números, são maiores que a média.
+# Escreva um algoritmo que peça ao usuário 5 números, calcule e mostre a média desses 
+#5 números e mostre ainda quantos, desses números, são maiores que a média.
 somaNumero = 0
 media = 0
 maiormedia = 0
+quantidadeDeNumeros = 5
+todosOsNumeros = []
+numerosMaioresQueAMedia = []
 
-for n in range(0,3):
-    print('{}° número'.format(n))
+for n in range(0,quantidadeDeNumeros): 
+    # Escrevendo o nome do numero que o usuario ira inserir EX: 1° número
+    print('{}° número'.format(n+1))
+    # Solicitando entrada de dados para o usuario
     numero = int(input('Digite um número: '))
+    # somaNumero = somaNumero + numero
     somaNumero += numero
-    if maiormedia > media:
-        
-        
-        
-        
-media = somaNumero / 3
-print ('A media dos seus 10 números é igual a:{} '.format(media))
+    # Adiciona numeros na lista de todos os numeros
+    todosOsNumeros.insert(n, numero)
+
+# Calcula a media
+media = somaNumero / quantidadeDeNumeros
+
+# Este for verifica quais os numeros sao maiores que a media
+for n in range(0,quantidadeDeNumeros): 
+    valorDaPosicao = todosOsNumeros[n]
+
+    if(valorDaPosicao > media):
+        numerosMaioresQueAMedia.append(valorDaPosicao)
+
+
+
+print('Essa é a lista {}'.format(todosOsNumeros))
+print('Essa é o valor da media {}'.format(media))
+print('Essa é a lista de numeros maiores que a media {}'.format(numerosMaioresQueAMedia))
